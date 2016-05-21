12372163d46e517a73428cea3d0d9ae877565747
Airiel D. Salvatore <airiel.d.salvatore@gmail.com>
2016-05-21 13:26:24 -0700

    can now specify color when creating balls

--- classes/ball.js
+++ classes/ball.js
@@ -16,7 +16,7 @@
   }
 
   GLB.Ball = {
-    create: function({ x, y, radius }){
+    create: function({ x, y, radius, color: { r, g, b, a } }){
       var ball = Object.create(BALL_PROTOTYPE);
 
       ball.location = GLB.Vector.create({x: x, y: y});
@@ -24,10 +24,6 @@
       ball.acceleration = GLB.Vector.create({x: 0, y: 0});
       ball.radius = radius || 20;
 
-      var r = Math.floor(Math.random() * 255);
-      var g = Math.floor(Math.random() * 255);
-      var b = Math.floor(Math.random() * 255);
-      var a = 0.2;
       ball.color = "rgba(" + r + ", " + g + ", " + b + ", " + a + ")";
 
       return ball;
--- examples/vectors/velocity/simulationLogic.js
+++ examples/vectors/velocity/simulationLogic.js
@@ -6,20 +6,20 @@
   var ball = GLB.Ball.create({
     x: 100,
     y: 100,
-    radius: 20
+    radius: 20,
+    color: {r: 50, g: 50, b: 50, a: .2}
   });
 
-  var ball2 = GLB.Ball.createRandom();
+  // var ball2 = GLB.Ball.createRandom();
 
   GLB.simulationLogic = {
     update: function () {
-      ball2.update();
-      console.log(ball2.location.x);
+      ball.update();
       GLB.Screen_edges.reflective_edges(ball)
     },
 
     draw: function () {
-      ball2.draw();
+      ball.draw();
     }
   }
 })();
