5ae387cb75435ea110e08e649b34a9d4fd759d31
Airiel D. Salvatore <airiel.d.salvatore@gmail.com>
2016-06-21 18:01:07 -0700

    example with forces effecting objects differently because of mass

---
+++ examples/forces/mass_and_forces/simulationLogic.js
@@ -0,0 +1,45 @@
+'use strict';
+
+(function(){
+  var GLB = window.GLB = window.GLB || {};
+
+  const STARTING_POINT_X = GLB.canvas.width / 5;
+  const STARTING_POINT_Y = GLB.canvas.height / 4;
+
+  const WIND = GLB.Vector.create({x: 10, y: 0});
+  const GRAVITY = GLB.Vector.create({x: 0, y: 0.01});
+
+  var balls = [];
+
+  _.times(10, function(){
+    var ball = GLB.Ball.create({
+      x: STARTING_POINT_X,
+      y: STARTING_POINT_Y,
+      radius: _.random(10, 30),
+      color: {r: 255, g: 127, b: 127, a: 0.2}
+    });
+
+    var GravitationalForce = GRAVITY.multiply(ball.mass);
+    var NetForce = GLB.Force.calculateForce([WIND, GravitationalForce]);
+    GLB.Force.applyNetForce({object: ball, NetForce});
+
+    balls.push(ball);
+  });
+
+
+  GLB.simulationLogic = {
+
+    update: function(){
+      _.forEach(balls, function(ball){
+        ball.update();
+        GLB.Screen_edges.reflect_off_edges(ball);
+      });
+    },
+
+    draw: function(){
+      _.forEach(balls, function(ball){
+        ball.draw();
+      });
+    },
+  }
+})();
