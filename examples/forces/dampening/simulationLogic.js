a1518923da9ecc942d2cf36bd132b98d589c4631
Airiel D. Salvatore <airiel.d.salvatore@gmail.com>
2016-06-23 16:12:25 -0700

    added an example showing velocity dampening

---
+++ examples/forces/dampening/simulationLogic.js
@@ -0,0 +1,47 @@
+'use strict';
+
+(function(){
+  var GLB = window.GLB = window.GLB || {};
+
+  const STARTING_POINT_X = GLB.canvas.width / 5;
+  const STARTING_POINT_Y = GLB.canvas.height / 4;
+
+  const WIND = GLB.Vector.create({x: 10, y: 0});
+  const GRAVITY = GLB.Vector.create({x: 0, y: 0.1});
+
+  var balls = [];
+
+  _.times(10, function(){
+    var ball = GLB.Ball.create({
+      x: STARTING_POINT_X,
+      y: STARTING_POINT_Y,
+      radius: _.random(10, 30)
+    });
+
+    ball.location.y = ball.location.y - ball.radius;
+
+    var GravitationalForce = GRAVITY.multiply(ball.mass);
+    var NetForce = GLB.Force.calculateForce([WIND, GravitationalForce]);
+    GLB.Force.applyNetForce({object: ball, NetForce});
+
+    balls.push(ball);
+  });
+
+
+  GLB.simulationLogic = {
+
+    update: function(){
+      _.forEach(balls, function(ball){
+        ball.velocity = ball.velocity.multiply(0.99);
+        ball.update();
+        GLB.Screen_edges.reflect_off_edges(ball);
+      });
+    },
+
+    draw: function(){
+      _.forEach(balls, function(ball){
+        ball.draw();
+      });
+    },
+  }
+})();
