00da94963a9674260a4b9b33bfdcd7f2b68fa1a7
Airiel D. Salvatore <airiel.d.salvatore@gmail.com>
2016-07-22 16:52:31 -0700

    set up templet for angular motion example

---
+++ examples/oscillation/simulationLogic.js
@@ -0,0 +1,16 @@
+'use strict';
+
+(function(){
+  var GLB = window.GLB = window.GLB = || {};
+
+  GLB.simulationLogic = function() {
+
+    update: function() {
+
+    },
+
+    draw: function() {
+
+    },
+  };
+})();
