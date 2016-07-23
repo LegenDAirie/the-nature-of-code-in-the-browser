66f4b5f9a9f244debf89c4c1c7898f0308163a20
Airiel D. Salvatore <airiel.d.salvatore@gmail.com>
2016-07-22 17:20:37 -0700

    moved the simulationLogic and html file into a folder called angular_motion and finished the example

---
+++ examples/oscillation/angular_motion/simulationLogic.js
@@ -0,0 +1,50 @@
+'use strict';
+
+(function(){
+  var GLB = window.GLB = window.GLB || {};
+
+  let middleX = GLB.canvas.width / 2;
+  let middleY = GLB.canvas.height / 2;
+
+  let ball1 = GLB.Ball.create({
+    x: -50,
+    y: 0,
+    radius: 10
+  });
+
+  let ball2 = GLB.Ball.create({
+    x: 50,
+    y: 0,
+    radius: 10
+  });
+
+  let angle = 0;
+  let angularVelocity = 0.02;
+
+  GLB.simulationLogic = {
+
+    init: function() {
+
+    },
+
+    update: function() {
+      angle = angle + angularVelocity;
+
+    },
+
+    draw: function() {
+      GLB.context.save();
+      GLB.context.translate(middleX, middleY);
+      GLB.context.rotate(angle);
+      ball1.draw();
+      ball2.draw();
+
+      GLB.Draw.line({
+        start: {x: ball1.location.x, y: ball1.location.y},
+        end: {x: ball2.location.x, y: ball2.location.y}
+      })
+
+      GLB.context.restore();
+    },
+  };
+})();
